01c729aa5d7957d31aba76668bec70c9c24cf910
mrbrunelli <matheus.brunelli@gmail.com>
2021-10-02 10:52:49 -0300

    implement example with timer decorator

---
+++ app.ts
@@ -0,0 +1,9 @@
+import { RandomDog } from "./modules/dog.service";
+
+async function main() {
+  const randomDog = new RandomDog();
+  const result = await randomDog.fetch();
+  console.log(result);
+}
+
+main();
