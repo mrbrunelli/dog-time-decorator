c2114f1b251b969826381859d55295149e6f0838
mrbrunelli <matheus.brunelli@gmail.com>
2021-10-02 11:20:05 -0300

    add space

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import { RandomDog } from "./modules/dog.service";
 
 async function main() {
   const randomDog = new RandomDog();
+
   const result = await randomDog.fetch();
   console.log(result);
 }
